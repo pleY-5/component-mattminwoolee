cae301fdc5172883e8dbf9fc513b8049b45d0a8e
nick567187 <nchang.edu@gmail.com>
2018-09-14 13:14:31 -0700

    cleaned up the code and removed user select bug

--- client/components/photo.jsx
+++ client/components/photo.jsx
@@ -5,8 +5,7 @@ class Photo extends React.Component {
   constructor(props) {
   	super(props);
   	this.state = {
-  	  caption: false,
-      once: true
+  	  caption: false
   	}
   }
 
@@ -14,16 +13,6 @@ class Photo extends React.Component {
   	this.setState({
   		caption: !this.state.caption
   	})
-  }
-
-  defaultHover() {
-    if(once) {
-      this.setState({
-        once: false
-      })
-    } else {
-      this.toggleHover();
-    }
   }
 
   render() {
@@ -37,7 +26,6 @@ class Photo extends React.Component {
   	var displayCaption = this.state.caption ? styles.spanShow : styles.spanHide;
     var imageAfter = this.state.caption ? styles.imageAfter : styles.image;
 
-
 	  if(this.props.users.length === 0) {
 		  return (
 		  	<div className={styles.photoContainer}>
@@ -48,7 +36,7 @@ class Photo extends React.Component {
     } else if (this.props.middle && this.props.shouldDefault) {
 	    return (
 		  	<div className={styles.photoContainer}>
-			    <img id={this.props.num} onMouseEnter={(e) => {this.defaultHover(); this.props.defaultPicture(e)}} onMouseLeave={(e) => {this.defaultHover(); this.props.defaultPicture(e)}} src={this.props.photo.url} 
+			    <img id={this.props.num} onMouseEnter={(e) => {this.toggleHover(); this.props.defaultPicture(e)}} onMouseLeave={(e) => {this.toggleHover(); this.props.defaultPicture(e)}} src={this.props.photo.url} 
 			    className={styles.imageAfter}/><span className={styles.spanShow}><img className={styles.avatar} src={this.props.users[userIndex].avatar}/>{this.props.photo.caption} {" "}<span className={styles.description}>by {this.props.users[userIndex].name}</span></span>
 		    </div>
 	    );
