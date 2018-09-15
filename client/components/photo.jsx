970896ed95f8f35ad5676dc8cf442af27f789e48
nick567187 <nchang.edu@gmail.com>
2018-09-14 18:05:20 -0700

    finished the layout of the modals as well as got them to match with the corresponding clicked pictures. definitely need to refactor away some of the mess I made while trying to get this feature working though

--- client/components/photo.jsx
+++ client/components/photo.jsx
@@ -29,22 +29,22 @@ class Photo extends React.Component {
 	  if(this.props.users.length === 0) {
 		  return (
 		  	<div className={styles.photoContainer}>
-			    <img id={this.props.num} onMouseEnter={(e) => {this.toggleHover(); this.props.defaultPicture(e)}} onMouseLeave={(e) => {this.toggleHover(); this.props.defaultPicture(e)}} src={this.props.photo.url} 
-			    className={imageAfter}/><span className={displayCaption}>{this.props.photo.caption}</span>
+			    <img id={this.props.num} name={`modal` + this.props.num} onMouseEnter={(e) => {this.toggleHover(); this.props.defaultPicture(e)}} onMouseLeave={(e) => {this.toggleHover(); this.props.defaultPicture(e)}} src={this.props.photo.url} 
+			    className={imageAfter} onClick={(e) => {this.props.showModal(e); this.props.detectModal(e)}}/><span className={displayCaption}>{this.props.photo.caption}</span>
 		    </div>
 		  );
     } else if (this.props.middle && this.props.shouldDefault) {
 	    return (
 		  	<div className={styles.photoContainer}>
-			    <img id={this.props.num} onMouseEnter={(e) => {this.toggleHover(); this.props.defaultPicture(e)}} onMouseLeave={(e) => {this.toggleHover(); this.props.defaultPicture(e)}} src={this.props.photo.url} 
-			    className={styles.imageAfter}/><span className={styles.spanShow}><img className={styles.avatar} src={this.props.users[userIndex].avatar}/>{this.props.photo.caption} {" "}<span className={styles.description}>by {this.props.users[userIndex].name}</span></span>
+			    <img id={this.props.num} name={`modal` + this.props.num} onMouseEnter={(e) => {this.toggleHover(); this.props.defaultPicture(e)}} onMouseLeave={(e) => {this.toggleHover(); this.props.defaultPicture(e)}} src={this.props.photo.url} 
+			    className={styles.imageAfter} onClick={(e) => {this.props.showModal(e); this.props.detectModal(e)}}/><span className={styles.spanShow}><img className={styles.avatar} src={this.props.users[userIndex].avatar}/>{this.props.photo.caption} {" "}<span className={styles.description}>by {this.props.users[userIndex].name}</span></span>
 		    </div>
 	    );
     } else {
       return (
         <div className={styles.photoContainer}>
-          <img id={this.props.num} onMouseEnter={(e) => {this.toggleHover(); this.props.defaultPicture(e)}} onMouseLeave={(e) => {this.toggleHover(); this.props.defaultPicture(e)}} src={this.props.photo.url} 
-          className={imageAfter}/><span className={displayCaption}><img className={styles.avatar} src={this.props.users[userIndex].avatar}/>{this.props.photo.caption} {" "}<span className={styles.description}>by {this.props.users[userIndex].name}</span></span>
+          <img id={this.props.num} name={`modal` + this.props.num} onMouseEnter={(e) => {this.toggleHover(); this.props.defaultPicture(e)}} onMouseLeave={(e) => {this.toggleHover(); this.props.defaultPicture(e)}} src={this.props.photo.url} 
+          className={imageAfter} onClick={(e) => {this.props.showModal(e); this.props.detectModal(e)}}/><span className={displayCaption}><img className={styles.avatar} src={this.props.users[userIndex].avatar}/>{this.props.photo.caption} {" "}<span className={styles.description}>by {this.props.users[userIndex].name}</span></span>
         </div>
       );
     }
