b9040b811c414eeab7a901db0bd24bd7738e5fca
Matthew Lee <Mattminwoolee@Matthews-MacBook-Pro-3.local>
2018-10-11 10:49:27 -0700

    Added loader.io

--- server/index.js
+++ server/index.js
@@ -2,14 +2,21 @@ require('dotenv').config();
 var express = require('express');
 var app = express();
 var parser = require('body-parser');
+var path = require('path');
 // var cors = require('cors');
+
+/********* Loader.io *********/
+app.get('/loaderio-581f8b7f8619e3d2c1c2cc8a79b5b3d9', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../public/loaderio-581f8b7f8619e3d2c1c2cc8a79b5b3d9.txt'));
+});
+
 app.use('/:idOrName', express.static('./public'));
 app.use(parser.json());
 app.use(parser.urlencoded({ extended: true }));
 
 
 
-/** Redis Cache **/
+/********* Redis Cache *********/
 // const redis = require('redis');
 // const client = redis.createClient(process.env.REDIS_REMOTE_PORT, process.env.REDIS_REMOTE_HOST);
 
@@ -29,16 +36,11 @@ app.use(parser.urlencoded({ extended: true }));
 // app.get('/api/photos/:idOrName/restaurants', cache);
 require('./../routes/routes.js')(app);
 
-/** Server side rendering **/
+/********* Server side rendering *********/
 // import React from 'react';
 // import { renderToString } from 'react-dom/server';
 // import App from './../client/index.jsx';
 // var Layout = require('./layout.js');
-
-// for loader.io
-// app.get('/loaderio-84dba0cf73601048ce62c9fe6114c0f8', (req, res) => {
-//   res.sendFile(path.resolve(__dirname, '../public/loaderio-84dba0cf73601048ce62c9fe6114c0f8.txt'));
-// });
 
 // var corsOptions = {
 //   origin: 'http://localhost:3005',
